826550426e3a85e68f931951a37cb05ebf99634c
Max Denushev <max@denushev.ru>
2022-04-24 21:53:23 +0700

    fix type

--- src/models/channel_notify_props.rs
+++ src/models/channel_notify_props.rs
@@ -9,13 +9,11 @@
  */
 
 
-
-
 #[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
 pub struct ChannelNotifyProps {
     /// Set to \"true\" to enable email notifications, \"false\" to disable, or \"default\" to use the global user notification setting.
     #[serde(rename = "email", skip_serializing_if = "Option::is_none")]
-    pub email: Option<bool>,
+    pub email: Option<String>,
     /// Set to \"all\" to receive push notifications for all activity, \"mention\" for mentions and direct messages only, \"none\" to disable, or \"default\" to use the global user notification setting.
     #[serde(rename = "push", skip_serializing_if = "Option::is_none")]
     pub push: Option<String>,
